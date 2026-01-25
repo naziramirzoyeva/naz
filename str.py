7f787673ec7d5ae1676a45d039b6130b6ec3828a
naziramirzoyeva <nazemirzoyeva@gmail.com>
2026-01-26 01:06:25 +0500

    add

---
+++ str.py
@@ -0,0 +1,24 @@
+#1
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+#2
+a = "Hello, World!"
+print(len(a))
+
+
+#3
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+#4
+w="I'm studying now"
+print(w[4::])
+
+#5
+a="Sometimes I get bored"
+print(a.replace("e","a"))
+print(a.lower())
