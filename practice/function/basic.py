3294f8aa22c0cffdc4cc144989e1a638b8ebe9e1
naziramirzoyeva <nazemirzoyeva@gmail.com>
2026-02-07 23:07:49 +0500

    Initial commit

---
+++ practice/function/basic.py
@@ -0,0 +1,34 @@
+#1
+def my_function():
+    print("Hello world")
+
+#2
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+
+
+#3
+
+
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
+
+#4
+def get_greeting():
+  return "Hello from a function"
+
+print(get_greeting())
+#5
+def get_greeting():
+  return "Hello from a function"
+
+message = get_greeting()
+print(message)
