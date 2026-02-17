d595f2a0105523dfb2908cb598f354d51f0eccf5
naziramirzoyeva <nazemirzoyeva@gmail.com>
2026-02-17 12:11:05 +0500

    Add Practice4

--- practice4/math.py/1.py
+++ practice4/math.py/1.py
@@ -1,4 +1,8 @@
 import math 
-degree=int(input("Degree: "))
+degree=float(input("Degree: "))
 radian=degree*math.pi/180
-print("Radian: ", radian)
+y= round(radian,6)
+print("Radian: ", y)
+import math
+
+
