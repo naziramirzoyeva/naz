f851a0358e94d16a4f38ef9ce5260eb14b9e2284
naziramirzoyeva <nazemirzoyeva@gmail.com>
2026-03-09 03:16:30 +0500

    Add

---
+++ practice_6/file_write.py
@@ -0,0 +1,8 @@
+file = open("sample.txt", "w")
+
+file.write("Hello, this is a sample file.\n")
+file.write("This file is used for Python practice.\n")
+
+file.close()
+
+print("File created and data written.")
